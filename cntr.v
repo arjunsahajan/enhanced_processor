7d8c6d8b9c143791fe017586d9d8f598b92015c5
arjunsahajan <arjunsahaj1996@gmail.com>
2023-06-14 00:02:39 -0500

    Update: made more simple, removed next state logic

--- cntr.v
+++ cntr.v
@@ -9,21 +9,16 @@ module cntr
 	output [15: 0] Q
 );
 
-	reg [15: 0] Q_reg, Q_nxt;
+	reg [15: 0] Q_reg;
 	
 	always @(posedge clk)
 	begin
-		Q_reg <= Q_nxt;
-	end
-	
-	always @(*)
-	begin
 		if(!reset_n)
-			Q_nxt <= 0;
+			Q_reg <= 0;
 		else if(!load)
-			Q_nxt <= D;
+			Q_reg <= D;
 		else if(en)
-			Q_nxt <= Q_nxt + 1;
+			Q_reg <= Q_reg + 1;
 	end
 	
 	assign Q = Q_reg;
